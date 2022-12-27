60ab82462e97526659a6e65a7ece03d0a01e3d16
Tim McCormack <cortex@brainonfire.net>
2022-12-26 19:53:10 -0500

    Bugfix: Go back to using Math.multiplyExact, which I had somehow lost

--- kotlin/src/main/kotlin/org/timmc/zaremba/Util.kt
+++ kotlin/src/main/kotlin/org/timmc/zaremba/Util.kt
@@ -12,4 +12,4 @@ fun <T> List<T>.swapAt(at: Int, mapper: (T) -> T): List<T> {
 }
 
 fun Iterable<Double>.product(): Double = fold(1.0, Double::times)
-fun Iterable<Long>.product(): Long = fold(1, Long::times)
+fun Iterable<Long>.product(): Long = fold(1L, Math::multiplyExact)
